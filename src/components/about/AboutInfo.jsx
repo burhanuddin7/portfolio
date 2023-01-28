65953aab5afbf3336c648632128a02b80ef60293
burhanuddin7 <madraswalaburhanuddin@gmail.com>
2023-01-28 11:49:53 +0530

    add profile image

--- src/components/about/AboutInfo.jsx
+++ src/components/about/AboutInfo.jsx
@@ -14,7 +14,7 @@ const AboutInfo = () => {
             <div className="about__box">
                 <i className="bx bx-briefcase-alt about__icon"></i>
                 <h3 className="about__title">Completed</h3>
-                <span className="about__subtext">10 + Projects</span>
+                <span className="about__subtext">20 + Projects</span>
             </div>
 
             <div className="about__box">
@@ -25,7 +25,7 @@ const AboutInfo = () => {
         </div>
 
         <p className="about__description">
-            Proficient in developing user interfaces and a firm believer in mobile first approach & good core web vitals numbers for the webpage. Team player with adequate management skills and a keen learner for trending technologies & frameworks to keep myself updated in ever growing software development industry.
+            Proficient in developing user interfaces and a firm believer in mobile first approach & good core web vitals numbers for the webpage. <br/>Team player with adequate management skills and a keen learner for trending technologies & frameworks to keep myself updated in ever growing software development industry.
         </p>
 
         <a rel="noreferrer" target="_blank" href={Resume} className="primary-btn about__btn">
